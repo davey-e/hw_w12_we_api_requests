250877f6a2d05ee7b0dda0c4049177c84d56c659
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-23 14:57:11 +0000

    Added displayCompanyInfo function to display data returned from getCompanyInfo in a table

--- public/app.js
+++ public/app.js
@@ -17,16 +17,47 @@ const requestComplete = function(){
     const jsonString = this.responseText; //This is a string
     const data = JSON.parse(jsonString); //This parses the string into a js object
     console.log(data);
-    
+    displayCompanyInfo(data);
 }
 
 const getCompanyInfo = function(){
     const stockNameInput = document.getElementById("stock-name-input");
     const company = stockNameInput.value;
-    console.log("company:",company);
     const url = "https://api.iextrading.com/1.0/stock/" + company + "/company";
-    console.log(url);
     makeRequest(url, requestComplete);
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const displayCompanyInfo = function(data){
+
+    const div = document.getElementById("company-info");
+    div.innerHTML = "";
+    const header = document.createElement("h1");
+    header.innerText = data.companyName;
+    div.appendChild(header);
+    const table = document.createElement("table");
+
+    for(let item in data){
+        if(item === "companyName"){
+            //dont do anything
+        } else if (item === "issueType"){
+            //don't do anything
+        } else {
+            const tr = document.createElement("tr");
+            const td1 = document.createElement("td");
+            const formattedItem = capitalizeFirstLetter(item);
+            td1.innerText = formattedItem + ":";
+            tr.appendChild(td1);
+            const td2 = document.createElement("td");
+            td2.innerText = data[item];
+            tr.appendChild(td2);
+            table.appendChild(tr);
+        }
+    };
+    div.appendChild(table);
 }
 
 
